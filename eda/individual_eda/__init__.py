3fbad4a8f8e694344ca8d76c3648e1ce4d0ceb36
Kaustubh Sonawane <ksstubh.sonawane@gmail.com>
2026-06-10 19:05:10 +0530

    Add 5_OmicsFusionFilteredSupplementary

---
+++ eda/individual_eda/__init__.py
@@ -0,0 +1,27 @@
+gene_expression_dir = "../../../data/raw/gene_expression/"
+gene_properties_dir = "../../../data/raw/gene_properties/"
+nomencleature_dir= "../../../data/raw/nomenclature/"
+non_gene_expression = "../../../data/raw/non_gene_expression/"
+
+
+# gene_expression
+df1 = f"{gene_expression_dir}1_4_hpa_rna_celline.tsv"
+df2 = f"{gene_expression_dir}2_DepMap_OmicsExpressionAllGenesTPMLogp1Profile.csv"
+df3 = f"{gene_expression_dir}3_GEOexpression.txt"
+df4 = f"{gene_expression_dir}4_Harmonized_MS_CCLE_Gygi_subsetted.csv"
+
+# gene_properties
+df5 = f"{gene_properties_dir}5_OmicsFusionFilteredSupplementary.csv"
+df6 = f"{gene_properties_dir}6_OmicsSomaticMutationsProfile.csv"
+
+# nomenclature
+df7 = f"{nomencleature_dir}7_cellosaurus.csv"
+df8 = f"{nomencleature_dir}8_DepMap_OmicsProfiles.csv"
+df9 = f"{nomencleature_dir}9_DepMap_sample_info.csv"
+df10 = f"{nomencleature_dir}10_GEOInfo.txt"
+df11 = f"{nomencleature_dir}11_hpa_rna_celline_description.tsv"
+
+# non_gene_expression
+df12 = f"{non_gene_expression}12_CCLE_metabolomics_20190502.csv"
+df13 = f"{non_gene_expression}13_CCLE_miRNA_20181103.gct"
+df14 = f"{non_gene_expression}14_OmicsGlobalSignatures.csv"
