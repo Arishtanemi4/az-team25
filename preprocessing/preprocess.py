ba262f2313b33b470f60413c0b656e654dbcd43a
Kaustubh Sonawane <ksstubh.sonawane@gmail.com>
2026-07-30 11:15:00 +0530

    calling resolve_duplicate_keys in preprocess.py

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -9,12 +9,16 @@ RAW = REPO_ROOT / "data" / "raw"
 
 PATHS = {
     "df7": RAW / "nomenclature" / "7_cellosaurus.csv",
+    "df9": RAW / "nomenclature" / "9_DepMap_sample_info.csv",
 }
 
 
 def main():
     df7_raw = pd.read_csv(PATHS["df7"])
     df7 = joins.filter_human(df7_raw)
+
+    df9 = pd.read_csv(PATHS["df9"])
+    df9, n_df9_dupes = joins.resolve_duplicate_keys(df9, ["DepMap_ID"], "RRID")
 
 
 if __name__ == "__main__":
