7a6cd890d8db4ef607307fb424bfb5566f042a7e
Chirag Sharma <chirag.sharma0086@gmail.com>
2026-08-07 15:30:00 +0530

    creating load_essentiality_constants

--- scoring/desirability.py
+++ scoring/desirability.py
@@ -14,3 +14,11 @@ EXTENDED_CONSTANTS_PATH = "scoring/resources/desirability_constants_extended.jso
 def load_extended_constants(path=EXTENDED_CONSTANTS_PATH):
     with open(path) as f:
         return json.load(f)
+
+
+ESSENTIALITY_CONSTANTS_PATH = "scoring/resources/common_essential_genes.json"
+
+
+def load_essentiality_constants(path=ESSENTIALITY_CONSTANTS_PATH):
+    with open(path) as f:
+        return json.load(f)
